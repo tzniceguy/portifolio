8df2ef3a90da4512a8cfd28c4037dd76b8b255bf
=tzniceguy <=joeshimbi@gmail.com>
2024-02-26 13:23:03 +0300

    centred footer with some padding and margin

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { CiHeart } from "react-icons/ci";
 
-
 export const Footer = () => {
   return (
-    <footer className='sticky bottom-0 p-4 inset-x-0'>
-    <h4 className="px-4 flex "> made with <CiHeart />
- by tzniceguy
-        </h4>
-</footer>
+    <footer className='fixed bottom-0 left-0 w-full bg-gray-200 p-4'>
+      <div className="mx-auto text-center">
+        <h4 className="inline-block">made with <CiHeart className="inline-block" /> by tzniceguy</h4>
+      </div>
+    </footer>
   )
 }
