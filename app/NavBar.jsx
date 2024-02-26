993db19c71e40569d94a44d6f1d411003438cb32
=tzniceguy <=joeshimbi@gmail.com>
2024-02-26 11:32:17 +0300

    added spacing and flex displa for medium screen

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -20,23 +20,23 @@ function NavBar() {
     ]
 
   return (
-    <nav  className="md:border-solid md:border-2 md:border-slate-100 md:rounded md:m-4 w-2/4 p-2 float-right sticky shadow-md">
+    <nav
+    className={`md:flex md:justify-between md:items-center md:space-x-6 md:border-solid md:border-2 md:border-slate-100 md:rounded md:m-4 w-2/4 p-2 float-right sticky shadow-md`}>
       <div className="flex justify-between float-right">
-      <button className="md:hidden hover:text-zinc-900 text-xl " onClick={toggleMenu}>
-        <CiMenuFries/>
-      </button>
-      <div className={`md:flex md:items-center md:space-x-6 ${menuOpen ? "block" : "hidden"} md:block`}>
-  <ul>
-    {links.map(link => (
-      <li key={link.href}>
-        <Link className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`} href={link.href}>
-          {link.label}
-        </Link>
-      </li>
-    ))}
-  </ul>
-</div>
-
+        <button className="md:hidden hover:text-zinc-900 text-xl " onClick={toggleMenu}>
+          <CiMenuFries/>
+        </button>
+        <div className={`md:flex md:items-center md:space-x-6 ${menuOpen ? "block" : "hidden"} md:block`}>
+          <ul className="md:flex md:items-center md:space-x-6 md:flex-row">
+            {links.map(link => (
+            <li key={link.href}>
+              <Link className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`} href={link.href}>
+                {link.label}
+              </Link>
+            </li>
+            ))}
+          </ul>
+        </div>  
       </div>
     </nav>
   ) 
