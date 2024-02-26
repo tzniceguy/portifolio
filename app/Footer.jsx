da32b9946c2f5221f8374ad2d5a0e948e3d82550
=tzniceguy <=joeshimbi@gmail.com>
2024-02-26 14:22:10 +0300

    modified footer

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -3,9 +3,11 @@ import { CiHeart } from "react-icons/ci";
 
 export const Footer = () => {
   return (
-    <footer className='fixed bottom-0 left-0 w-full bg-gray-200 p-4'>
-      <div className="mx-auto text-center">
-        <h4 className="inline-block">made with <CiHeart className="inline-block" /> by tzniceguy</h4>
+    <footer className="py-4 border-t border-gray-200 dark:border-gray-800">
+      <div className='fixed bottom-0 left-0 w-full p-4'>
+        <div className="mx-auto text-center">
+          <h4 className="inline-block">made with <CiHeart className="inline-block" /> by tzniceguy</h4>
+        </div>
       </div>
     </footer>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,9 @@ export default function Home() {
         <div className="grid max-w-3xl gap-8 mx-auto items-center space-y-4 lg:gap-16 lg:max-w-5xl lg:grid-colos-2 lg:space-y-0">
           <div className="space-y-4 text-center lg:order-2 lg-space-y-3 xl:space-y-4">
             <div className="space-y-2">
-              <h1 className=" text-xl font-semibold"> Hello I am  Tzniceguy,</h1>
-              <p className="inline-block text-2xl font-semibold tracking-wide md:text-3xl lg:text-4xl">
-                Developer
+              <h1 className=" text-4xl font-semibold">   Tzniceguy</h1>
+              <p className="inline-block text-2xl  tracking-wide md:text-3xl lg:text-4xl">
+                Developer / Human
               </p>
             </div>
             <div className="mx-auto grid max-w-sm items-start gap-4 text-left sm:max-w-2xl md:gap-6">
